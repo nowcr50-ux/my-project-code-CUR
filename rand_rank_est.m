ec84d3f732c827a5cbb7908c8e04d990151e56b7
nowcr50-ux <nowcr50@gmail.com>
2026-05-21 23:17:51 +0100

    rand_rank_est.m

--- rand_rank_est.m
+++ rand_rank_est.m
@@ -19,7 +19,7 @@ function [r_hat, X] = rand_rank_est(A, eps_tol, s0, s_step)
         normA_est = sv(1);
         r_hat = sum(sv > eps_tol * normA_est);
 
-        r_hat = min(r_hat, min(size(A))); %added this line
+        r_hat = min(r_hat, min(size(A))); 
 
         if sv(end) < eps_tol * normA_est
              break

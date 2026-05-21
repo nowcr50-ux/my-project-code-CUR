1ae9fe71c25c05f46c05718313165dc3502736bf
nowcr50-ux <nowcr50@gmail.com>
2026-05-21 23:17:14 +0100

    Pivot_Residual.m

--- Pivot_Residual.m
+++ Pivot_Residual.m
@@ -1,8 +1,8 @@
 function [Is,Js] = Pivot_Residual(A,XR,I,J)
 [s,~]= size(XR);
 Js = CPQR(XR);
-Js = Js(~ismember(Js, J)); %added
-Js = Js(1:s); %original
+Js = Js(~ismember(Js, J)); 
+Js = Js(1:s); 
 
 C = A(:,J);
 U = A(I,J);
@@ -12,6 +12,6 @@ Cr = C *(U\Rjs);
 CR = A(:,Js) - Cr;
 
 Is = CPQR(CR');
-Is = Is(~ismember(Is, I)); %added
-Is = Is(1:s); %original
+Is = Is(~ismember(Is, I)); 
+Is = Is(1:s); 
 end
